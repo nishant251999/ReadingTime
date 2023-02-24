accab90f25eee83251fe4804c2598ebcb3535120
Nishant Jain <nishantjain251999@gmail.com>
2023-02-24 11:48:38 +0530

    first commit

---
+++ scripts/content.js
@@ -0,0 +1,19 @@
+const article = document.querySelector("article");
+
+if(article) {
+    const text = article.textContent;
+    const wordMatchRegExp = /[^\s]+/g; // Regular expression
+    let words = text.matchAll(wordMatchRegExp);
+    words = [...words];
+    const readingTime = Math.round(words.length/200);
+    const readingTimeInSec = Math.round(words.length*60/200);
+
+    const badge = document.createElement("p");
+    badge.classList.add("timeBadge", "color-secondary-text", "type--caption");
+    badge.textContent = `⏱️ ${readingTime} min Read`;
+    const time = article.querySelector(".flow-space-200.stack time")?.parentNode;
+    const heading = article.querySelector("h1");
+    (time??heading).insertAdjacentElement("afterend", badge);
+
+}
+
